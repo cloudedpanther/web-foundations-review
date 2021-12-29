c719c9c2ba258bc765fbf46bfc6eaf2da1541a65
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-12-30 05:06:16 +0900

    regular expressions

--- grade-2/js/animation.js
+++ grade-2/js/animation.js
@@ -13,3 +13,32 @@ $(".show-modal").on("click", function () {
 $(".modal-close").on("click", function () {
   $(".modal").css("transform", "translateY(-100%)");
 });
+
+const emailRegex =
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+$(".modal-form").on("submit", function (e) {
+  const emailValue = $(".modal-form .email").val();
+  const isEmailEmpty = emailValue === "";
+  const emailValid = emailRegex.test(emailValue);
+  if (isEmailEmpty) {
+    e.preventDefault();
+    $(".emailWarning").html("Email is empty!");
+    $(".emailWarning").css("display", "block");
+  } else if (!emailValid) {
+    e.preventDefault();
+    $(".emailWarning").html("Something wrong with your email form.");
+    $(".emailWarning").css("display", "block");
+  }
+});
+
+const pwRegex = /[A-Z]/;
+
+$(".modal-form").on("submit", function (e) {
+  const pwValue = $(".modal-form .password").val();
+  const pwValid = pwRegex.test(pwValue);
+  if (!pwValid) {
+    e.preventDefault();
+    $(".pwWarning").css("display", "block");
+  }
+});
